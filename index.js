d6f57683d2cee2f8e29c6a464839f1e92765a9a5
samiullah342317 <samiullah34231@gmail.com>
2025-11-02 22:33:12 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const VERIFY_TOKEN = "my_secret_token";
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+  if (mode && token === VERIFY_TOKEN) res.status(200).send(challenge);
+  else res.sendStatus(403);
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("Received:", JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log("Webhook running on port 3000"));
